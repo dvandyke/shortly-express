b98d8187acca7605c748a71327a55893933aab89
dvandyke <haoled@gmail.com>
2015-03-02 15:54:23 -0800

    Implement basic routing for new user
    - Added user table to sqlite.

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,13 @@ var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
-var User = db.Model.extend({
-});
+var User = db.Model.extend({});
+
+User.encrypt = function(obj){
+  bcrypt.hash(obj.password, null, null, function(err, hash){
+    console.log(hash);
+  })
+};
+
 
 module.exports = User;
--- shortly.js
+++ shortly.js
@@ -23,24 +23,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/', 
+app.get('/',
+function(req, res) {
+  // res.render('index');
+  res.render('login');
+});
+
+app.get('/create',
 function(req, res) {
   res.render('index');
 });
 
-app.get('/create', 
-function(req, res) {
-  res.render('index');
+// Routing request to create accounts to signup.ejs
+app.get('/signup',
+function(req,res) {
+  res.render('signup');
 });
 
-app.get('/links', 
+app.get('/links',
 function(req, res) {
   Links.reset().fetch().then(function(links) {
     res.send(200, links.models);
   });
 });
 
-app.post('/links', 
+app.post('/links',
 function(req, res) {
   var uri = req.body.url;
 
@@ -77,6 +84,11 @@ function(req, res) {
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
+app.post('/signup',
+  function(req, res){
+    // console.log(db.Model);
+    User.encrypt(req.body);
+  })
 
 
 
